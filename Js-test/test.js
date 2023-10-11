c751106ecf356d45e4f47d9c2ed6c4f9eac85897
dans <dansdana94@gmail.com>
2023-10-11 07:54:29 +0100

    saves

---
+++ Js-test/test.js
@@ -0,0 +1,16 @@
+let dans = "use of english";
+bad = dans ;
+let bas = `you can ${dans} the cup`;
+console.log(bas);
+let say = "say"
+let you = "what you want"
+let join = `${say} ${you}`
+console.log(join)
+
+const button =
+document.querySelector("whats your name")
+
+const what = "she said \"i refuse\""
+console.log(what) ;
+let len = dans.substring(4,14) ;
+console.log(len)
